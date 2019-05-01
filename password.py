0b95d15207768e6ec40bfbde56ee6109ff3afd5c
sprhawk <465558+sprhawk@users.noreply.github.com>
2019-05-02 05:22:49 +0800

    use argument to control behavior

--- password.py
+++ password.py
@@ -4,7 +4,53 @@
 
 import random
 
-s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
-passlen = 12
-p =  "".join(random.sample(s,passlen ))
-print(p)
+SYMBOL = "!@#$%^&*()?_-\;'"
+UPPERCASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+LOWERCASE = 'abcdefghijklmnopqrstuvwxyz'
+DIGIT = '0123456789'
+
+DEFAULT_LEN = 12
+
+def generate(sample, length):
+    random.seed()
+    # p =  "".join(random.sample(sample,length ))
+    p = random.choices(sample, k=length)
+    p = "".join(p)
+    return p
+    
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+
+    group = parser.add_mutually_exclusive_group()
+
+    group.add_argument('-a', '--alphabet', help='[a-zA-Z]', action='store_true')
+    group.add_argument('-u', '--uppercase', help='[A-Z]', action='store_true')
+    group.add_argument('-l', '--lowercase', help='[a-z]', action='store_true')
+    parser.add_argument('-s', '--symbol', help=r'symbols', action='store_true')
+    parser.add_argument('-d', '--digit', help='[0-9]', action='store_true')
+    parser.add_argument('-c', '--count', type=int, help='length of generated code')
+
+    # parser.set_defaults(alphabet=True, symbol=True, digit=True, count=DEFAULT_LEN)
+    parser.set_defaults(count=DEFAULT_LEN)
+    args = parser.parse_args()
+
+    samples = []
+    if args.alphabet:
+        samples.append(UPPERCASE)
+        samples.append(LOWERCASE)
+    elif args.uppercase:
+        samples.append(UPPERCASE)
+    elif args.lowercase:
+        samples.append(LOWERCASE)
+
+    if args.digit:
+        samples.append(DIGIT)
+
+    if args.symbol:
+        samples.append(SYMBOL)
+
+    sample = "".join(samples)
+
+    p = generate(sample, args.count)
+    print(p)
